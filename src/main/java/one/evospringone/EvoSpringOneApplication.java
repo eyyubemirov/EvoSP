e25ea783fb193da32c5ecfa4e23eabe206de8303
Eyyub Emirov <eyyubemirov9a@gmail.com>
2023-06-09 00:58:08 +0400

    proyekt baslangici

---
+++ src/main/java/one/evospringone/EvoSpringOneApplication.java
@@ -0,0 +1,13 @@
+package one.evospringone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EvoSpringOneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EvoSpringOneApplication.class, args);
+    }
+
+}
---
+++ src/test/java/one/evospringone/EvoSpringOneApplicationTests.java
@@ -0,0 +1,13 @@
+package one.evospringone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EvoSpringOneApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
